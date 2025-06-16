25d7a252525b722413aa45e2138bd7eefc6d54b3
NGUYEN Phuc Hung <hung.np@cyberlogitec.com>
2025-06-16 21:37:04 +0700

    update gitignore

--- src/dtos/schema.dto.ts
+++ src/dtos/schema.dto.ts
@@ -1,76 +1,40 @@
-import {
-  IsArray,
-  IsIn,
-  IsOptional,
-  IsString,
-  ValidateNested,
-} from "class-validator";
-import { Type } from "class-transformer";
 import { PropertyType } from "../../core/parser";
 
-const propertyType: PropertyType[] = ["number", "text"];
-
 export class SchemaRootDto {
-  @IsString()
   dbname!: string;
-
-  @Type(() => CollectionSchemaDto)
   collections!: CollectionSchemaDto[];
 }
 
 export class CollectionSchemaDto {
-  @IsString()
   name!: string;
-
-  @ValidateNested({ each: true })
-  @IsArray()
-  @Type(() => FieldPropertiesDto)
   fields!: FieldPropertiesDto[];
-
-  @ValidateNested()
-  @IsArray()
-  @Type(() => SchemaReferenceDto)
   references?: SchemaReferenceDto[];
 }
 
 export class SchemaReferenceDto {
-  @IsString()
   key!: string;
-  @IsString()
   targetTable!: string;
-  @IsString()
   targetKey!: string;
 }
 
 export class FieldPropertiesDto {
-  @IsString()
   name!: string;
 
-  @IsOptional()
   primarykey?: boolean;
 
-  @IsOptional()
   autoincreasement?: boolean;
 
-  @IsString()
-  @IsIn(propertyType)
   type!: PropertyType;
 
-  @IsOptional()
   required?: boolean;
 
-  @IsOptional()
   unique?: boolean;
 
-  @IsOptional()
   oncreate?: boolean;
 
-  @IsOptional()
   onupdate?: boolean;
 
-  @IsOptional()
   presentable?: boolean;
 
-  @IsOptional()
   default?: any;
 }
